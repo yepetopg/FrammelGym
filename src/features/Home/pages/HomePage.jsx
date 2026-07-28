30d855db55db6c56a0fa91807eed8df53eaa1cf3
yepetopg <yepeto.progamers@gmail.com>
2026-07-28 18:55:02 -0500

    feat(navbar): Agregar el navbar en main

--- src/features/Home/pages/HomePage.jsx
+++ src/features/Home/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import { NavBar } from "../../../shared/components/layout/NavBar/NavBar";
-import BurgerMenu from '../../../shared/components/ui/BurgerMenu/BurgerMenu'
 
 export default function HomePage() {
     return (
