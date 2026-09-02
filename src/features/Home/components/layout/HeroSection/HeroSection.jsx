71f186bbfd1f3698e6138df57621ee5a91c9dd13
nessunodev <jsbp2131@gmail.com>
2026-09-02 07:37:49 -0500

    feat(home): improve hero section structure and styling

--- src/features/Home/components/layout/HeroSection/HeroSection.jsx
+++ src/features/Home/components/layout/HeroSection/HeroSection.jsx
@@ -2,31 +2,37 @@ import heroSectionStyles from './heroSection.module.css'
 
 export const HeroSection = () => {
     return (
-        <section className={heroSectionStyles.heroSection}>
-            <div className={heroSectionStyles.heroContent}>
-             
-                 <h1>
-                    El gimnasio donde tu edad no importa, solo tu{' '}
-                    <span>decisión.</span>
+        <section className={heroSectionStyles.heroSection} >
+            <div className={heroSectionStyles.heroInner}>
+
+            <article className={heroSectionStyles.heroContent}>
+             <h1>
+                    El gimnasio donde tu edad no importa, solo tu 
+                    <span> decisión.</span>
                 </h1>
+
                 <p>
                     Entrena acompañado y sin limitaciones. Más de 15 años
                     formando personas en Santa Rosa de Osos con experiencia
                     profesional e integral.
                 </p>
-            <div className={heroSectionStyles.heroActions}>
+                </article>
+
+            <article className={heroSectionStyles.heroActions}>
                     <a href="#contacto">
-                        Inscribirme ahora →
+                        Inscribirme ahora 
+                        <img src='/arrow-right.svg' alt=''/>
                     </a>
 
                     <a href="#planes">
                         Ver planes
                     </a>
-                </div>
+                </article>
+
                  <div className={heroSectionStyles.heroAccent}></div>
 
-                
-            </div>
+                 </div>
+
              <div className={heroSectionStyles.heroGlow}></div>
             
         </section>
