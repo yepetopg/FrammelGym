d5354ceb897f12ed6087a38f2a19782d35ff259f
nessunodev <jsbp2131@gmail.com>
2026-09-15 01:12:17 -0500

    refactor(hero-section): quitar barra decorativa

--- src/features/Home/components/layout/HeroSection/HeroSection.jsx
+++ src/features/Home/components/layout/HeroSection/HeroSection.jsx
@@ -29,7 +29,7 @@ export const HeroSection = () => {
                     </a>
                 </article>
 
-                 <div className={heroSectionStyles.heroAccent}></div>
+                 
 
                  </div>
 
