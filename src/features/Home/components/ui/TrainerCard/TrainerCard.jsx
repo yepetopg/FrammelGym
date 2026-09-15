9be11dd64ab924d759011dddbeb3439cd6986118
nessunodev <jsbp2131@gmail.com>
2026-09-15 03:13:52 -0500

    feat(trainer-section): agregar sección de entrenadores

---
+++ src/features/Home/components/ui/TrainerCard/TrainerCard.jsx
@@ -0,0 +1,23 @@
+import trainerCardStyles from './TrainerCard.module.css'
+
+const TrainerCard = ({ name, role, phrase, image }) => {
+    return (
+        <article className={trainerCardStyles.card}>
+            <img
+                className={trainerCardStyles.image}
+                src={image}
+                alt={name}
+            />
+
+            <div className={trainerCardStyles.info}>
+                <h3 className={trainerCardStyles.name}>{name}</h3>
+
+                <p className={trainerCardStyles.role}>{role}</p>
+
+                <p className={trainerCardStyles.phrase}>{phrase}</p>
+            </div>
+        </article>
+    )
+}
+
+export default TrainerCard
